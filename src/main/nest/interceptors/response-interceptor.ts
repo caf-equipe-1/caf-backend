547e173cfa2aac387d62dd80ab455e064f03aa45
Douglas Volcato <douglasvolcato@gmail.com>
2023-04-11 20:32:15 -0300

    feat(main):add nest http response status code interceptor

---
+++ src/main/nest/interceptors/response-interceptor.ts
@@ -0,0 +1,19 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  HttpException,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      tap((data) => {
+        throw new HttpException(data, data.statusCode);
+      }),
+    );
+  }
+}
