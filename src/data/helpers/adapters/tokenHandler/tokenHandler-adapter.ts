623bd24b763e14db41b20546f4d7de55a931f04c
Douglas Volcato <douglasvolcato@gmail.com>
2023-04-10 21:05:00 -0300

    feat(data): add token handler adapter

---
+++ src/data/helpers/adapters/tokenHandler/tokenHandler-adapter.ts
@@ -0,0 +1,40 @@
+import * as jwt from 'jsonwebtoken';
+import { TokenHandlerAdapterInterface } from 'src/data/abstract/helpers/tokenHandler/tokenHandler-adapter-interface';
+import { GetOneUserUsecase } from 'src/data/usecases/user/getOneUser-usecase';
+import { User } from 'src/domain/entities/user/user-entity';
+import { InvalidParamError } from 'src/utils/errors/invalidParam-error';
+
+export class TokenHandlerAdapter implements TokenHandlerAdapterInterface {
+  public constructor(private readonly getOneUserUsecase: GetOneUserUsecase) {}
+
+  public generateToken(userId: string, secret: string): string {
+    return jwt.sign({ id: userId }, secret, {
+      expiresIn: 86400,
+    });
+  }
+
+  public async validateToken(token: string, secret: string): Promise<User> {
+    let mainUser: User;
+    await jwt.verify(token, secret, async (error, decoded: User) => {
+      try {
+        if (error) {
+          throw new InvalidParamError('Token');
+        }
+
+        const userId = decoded.id;
+
+        const user = await this.getOneUserUsecase.execute(userId);
+
+        if (!user || !user.id) {
+          throw new InvalidParamError('token');
+        }
+
+        mainUser = user;
+        return;
+      } catch (error) {
+        throw new InvalidParamError('token');
+      }
+    });
+    return mainUser;
+  }
+}
