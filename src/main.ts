8679e40ee672a8e5f04bd9a00f1568806b876cc8
Nayahto <nayahto157@gmail.com>
2023-04-12 10:35:00 -0300

    feat(swagger): add  swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { EnvVariables } from './utils/env/envVariables-util';
 import { MainModule } from './main/nest/modules/main/main.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
 
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('FaceApp')
+    .setDescription(
+      'APi with nestJs as a framework and typescript as main development language',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(EnvVariables.getPort());
 }
