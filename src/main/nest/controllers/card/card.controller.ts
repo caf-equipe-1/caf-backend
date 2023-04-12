3bd2087633ab1c70b6de0b4f72a4154fe934bb22
Douglas Volcato <douglasvolcato@gmail.com>
2023-04-11 21:00:34 -0300

    feat(main): add controllers

---
+++ src/main/nest/controllers/card/card.controller.ts
@@ -0,0 +1,75 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { HttpRequest } from 'src/domain/dtos/http/http-request-dto';
+import { CreateOrUpdateCardType } from 'src/domain/types/entities/card/createOrUpdateCard-type';
+import { CreateCardController } from 'src/presentation/controllers/card/createCard-controller';
+import { DeleteCardController } from 'src/presentation/controllers/card/deleteCard-controller';
+import { GetCardController } from 'src/presentation/controllers/card/getCard-controller';
+import { UpdateCardController } from 'src/presentation/controllers/card/updateCard-controller';
+
+@Controller('/card')
+export class CardController {
+  public constructor(
+    private readonly createCardController: CreateCardController,
+    private readonly updateCardController: UpdateCardController,
+    private readonly deleteCardController: DeleteCardController,
+    private readonly getCardController: GetCardController,
+  ) {}
+
+  @Post()
+  public async create(@Body() body: CreateOrUpdateCardType) {
+    const requestBody: any = body;
+    const userId = requestBody.userId;
+    const httpRequest: HttpRequest<CreateOrUpdateCardType> = { userId, body };
+
+    return await this.createCardController.execute(httpRequest);
+  }
+
+  @Patch(':id')
+  public async update(
+    @Param('id') id: string,
+    @Body() body: CreateOrUpdateCardType,
+  ) {
+    const requestBody: any = body;
+    const userId = requestBody.userId;
+    const httpRequest: HttpRequest<CreateOrUpdateCardType> = {
+      userId,
+      id,
+      body,
+    };
+
+    return await this.updateCardController.execute(httpRequest);
+  }
+
+  @Delete(':id')
+  public async delete(@Param('id') id: string, @Body() body: any) {
+    const requestBody: any = body;
+    const userId = requestBody.userId;
+    const httpRequest: HttpRequest<{}> = { userId, id };
+
+    return await this.deleteCardController.execute(httpRequest);
+  }
+
+  @Get(':id')
+  public async getOne(@Param('id') id: string) {
+    const httpRequest: HttpRequest<{}> = { id };
+
+    return await this.getCardController.execute(httpRequest);
+  }
+
+  @Get()
+  public async getAll(@Body() body: any) {
+    const requestBody: any = body;
+    const userId = requestBody.userId;
+    const httpRequest: HttpRequest<{}> = { userId };
+
+    return await this.getCardController.execute(httpRequest);
+  }
+}
---
+++ src/main/nest/controllers/document/document.controller.ts
@@ -0,0 +1,78 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { HttpRequest } from 'src/domain/dtos/http/http-request-dto';
+import { CreateOrUpdateDocumentType } from 'src/domain/types/entities/document/createOrUpdateDocument-type';
+import { CreateDocumentController } from 'src/presentation/controllers/document/createDocument-controller';
+import { DeleteDocumentController } from 'src/presentation/controllers/document/deleteDocument-controller';
+import { GetDocumentController } from 'src/presentation/controllers/document/getDocument-controller';
+import { UpdateDocumentController } from 'src/presentation/controllers/document/updateDocument-controller';
+
+@Controller('/document')
+export class DocumentController {
+  public constructor(
+    private readonly createDocumentController: CreateDocumentController,
+    private readonly updateDocumentController: UpdateDocumentController,
+    private readonly deleteDocumentController: DeleteDocumentController,
+    private readonly getDocumentController: GetDocumentController,
+  ) {}
+
+  @Post()
+  public async create(@Body() body: CreateOrUpdateDocumentType) {
+    const requestBody: any = body;
+    const userId = requestBody.userId;
+    const httpRequest: HttpRequest<CreateOrUpdateDocumentType> = {
+      userId,
+      body,
+    };
+
+    return await this.createDocumentController.execute(httpRequest);
+  }
+
+  @Patch(':id')
+  public async update(
+    @Param('id') id: string,
+    @Body() body: CreateOrUpdateDocumentType,
+  ) {
+    const requestBody: any = body;
+    const userId = requestBody.userId;
+    const httpRequest: HttpRequest<CreateOrUpdateDocumentType> = {
+      userId,
+      id,
+      body,
+    };
+
+    return await this.updateDocumentController.execute(httpRequest);
+  }
+
+  @Delete(':id')
+  public async delete(@Param('id') id: string, @Body() body: any) {
+    const requestBody: any = body;
+    const userId = requestBody.userId;
+    const httpRequest: HttpRequest<{}> = { userId, id };
+
+    return await this.deleteDocumentController.execute(httpRequest);
+  }
+
+  @Get(':id')
+  public async getOne(@Param('id') id: string) {
+    const httpRequest: HttpRequest<{}> = { id };
+
+    return await this.getDocumentController.execute(httpRequest);
+  }
+
+  @Get()
+  public async getAll(@Body() body: any) {
+    const requestBody: any = body;
+    const userId = requestBody.userId;
+    const httpRequest: HttpRequest<{}> = { userId };
+
+    return await this.getDocumentController.execute(httpRequest);
+  }
+}
