e88d0101b99a858193c5149e9be1f9ffd9ff963e
Douglas Volcato <douglasvolcato@gmail.com>
2023-04-12 22:49:29 -0300

    feat(main): add nest login controllers

---
+++ src/main/nest/controllers/login/makeEmailLogin.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { EmailLoginDto } from 'src/domain/dtos/login/emailLogin-dto';
+import { EmailLoginController } from 'src/presentation/controllers/login/emailLogin-controller';
+
+@ApiTags('EmailLogin')
+@Controller('/login/email')
+export class MakeEmailLoginController {
+  public constructor(
+    private readonly emailLoginController: EmailLoginController,
+  ) {}
+
+  public async login(@Body() body: EmailLoginDto) {
+    return await this.emailLoginController.execute({ body });
+  }
+}
---
+++ src/main/nest/controllers/login/makeSelfieLogin.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { SelfieLoginDto } from 'src/domain/dtos/login/selfieLogin-dto';
+import { SelfieLoginController } from 'src/presentation/controllers/login/selfieLogin-controller';
+
+@ApiTags('SelfieLogin')
+@Controller('/login/selfie')
+export class MakeSelfieLoginController {
+  public constructor(
+    private readonly selfieLoginController: SelfieLoginController,
+  ) {}
+
+  public async login(@Body() body: SelfieLoginDto) {
+    return await this.selfieLoginController.execute({ body });
+  }
+}
