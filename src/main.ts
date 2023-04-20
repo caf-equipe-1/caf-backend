ccaf4b692a522818d9b41a6560689d99e46ed844
Douglas Volcato <douglasvolcato@gmail.com>
2023-04-20 20:52:40 -0300

    feat(main): increase api request size for larger files

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { EnvVariables } from './utils/env/envVariables-util';
 import { MainModule } from './main/nest/modules/main/main.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
 
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('FaceApp')
     .setDescription('APi made with Typescript and NestJS.')
